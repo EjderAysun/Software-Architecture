275f9c68f129f0bd8677d0289f66caafb895e2a2
Ejder Aysun <ejderaysunn@gmail.com>
2024-03-01 01:59:13 +0300

    Added the Singleton Pattern with its basic explanation via code

---
+++ The_Singleton_Pattern/Test.java
@@ -0,0 +1,19 @@
+package The_Singleton_Pattern;
+
+public class Test {
+    public static void main(String[] args) {
+        // Here is a test program:
+        X x1 = null, x2 = null;
+        x1 = X.getInstance();
+        System.out.println("First reference: <" + x1 + ">");
+        x2 = X.getInstance();
+        System.out.println("Second reference: <" + x2 + ">");
+    }
+}
+
+// NOTE: To make the code more understandable, you can put "Instance" word instead of every "X" word.
+
+/* Output:
+    First reference: <The_Singleton_Pattern.RefA>
+    Second reference: <The_Singleton_Pattern.RefA>
+*/
---
+++ The_Singleton_Pattern/X.java
@@ -0,0 +1,21 @@
+package The_Singleton_Pattern;
+
+public class X {
+    // The private reference to the one and only instance.
+    private static X uniqueStaticPrivateXInstance;
+    
+    // The X Constructor. It is private!
+    // No client can instantiate a Singleton object!
+    private X() {}
+
+    // Returns a reference to the single instance.
+    // No client can instantiate a X object!
+    public static X getInstance() {
+        if(uniqueStaticPrivateXInstance == null) {
+            uniqueStaticPrivateXInstance = new X();
+        }
+        return uniqueStaticPrivateXInstance;
+    }
+}
+
+// NOTE: To make the code more understandable, you can put "Instance" word instead of every "X" word.
