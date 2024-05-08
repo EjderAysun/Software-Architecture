9a1b53c24f64a8b7ec422800a93cd7be91194997
Ejder Aysun <ejderaysunn@gmail.com>
2024-05-08 16:41:42 +0300

    Added Class Adapter Pattern

---
+++ a8_Adapter_Pattern/Adapter_Pattern_Implementation/ClassAdapterPattern.java
@@ -0,0 +1,49 @@
+package a8_Adapter_Pattern.Adapter_Pattern_Implementation;
+
+// This is the "Target" class.
+// North America Socket.
+// Our device is manufactured for to be
+// used with a North American Socket.
+interface NASocket {
+    int usingNASocket();
+}
+
+// This is the "Adaptee" class.
+// Continential Europe Socket.
+// The device will be used in
+// Europe which has different sockets.
+class EURSocket {
+    public int usingEURSocket() {
+        System.out.println("Giving you 220 Volt using Europe Connection.");
+        return 220;
+    }
+}
+
+// This is the "Adapter" class. 
+// ConnectorAdapterNAtoEUR.
+// We need a connector so our device works.
+class ConnectorAdapterNAtoEUR extends EURSocket implements NASocket {
+    public int usingNASocket() {
+        // Possibly do some other work and then call
+        // usingEURSocket from the European socket.
+        int voltage = usingEURSocket();
+        return voltage;
+    }
+}
+
+// Utility Class
+class VCR {
+    public void powerUp(int voltage) {System.out.println("Powered up to " + voltage + " Volt.");}
+}
+
+public class ClassAdapterPattern {
+    public static void main(String[] args) {
+        // Create the adapter.
+        NASocket socket = new ConnectorAdapterNAtoEUR();
+        // socket is-a North American socket.
+        // So our North American device can connect.
+        int voltage = socket.usingNASocket();
+        VCR vcr = new VCR();
+        vcr.powerUp(voltage);
+    }
+}
